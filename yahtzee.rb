03ffd1be4d2fac752e6af89031e735f26b53d35a
bondparkerbond <parkerbond@gmail.com>
2015-08-24 15:09:48 -0600

    start of scoring type methods

--- yahtzee.rb
+++ yahtzee.rb
@@ -58,6 +58,7 @@ end #end of turn method
 end #end of game class
 
 
+
 class Player
   attr_accessor :name, :scorecard
 
@@ -66,10 +67,17 @@ class Player
     @scorecard = []
   end #end of player class initialize method
 
+  def add_players
+    #will add in ability to add more players and name
+    player_1 = "Parker"
+  end #end of add player method
+
   def calculate_score
     #to be written later
   end # end of method calculate_score
 end #end of player class
+
+
 
 class ScoreCard
   attr_accessor :scorecard, :scoring_options, :total_score
@@ -116,10 +124,69 @@ class ScoreCard
     @total_score = 0
   end #end of initialize method
 
-  # def aces(roll)
-  #   num_of_aces = 
-  #   @total_score += num_of_aces
-  # end
+  def testing
+    puts "Test Script"
+    #puts "#{@scorecard}"
+    puts "#{@scoring_options}"
+  end #end of testing method
+
+#Upper Section Methods
+  def aces(roll)
+    num_of_aces = 
+    @total_score += num_of_aces
+  end
+
+  def twos(roll)
+    #to add in later
+  end #end of twos method
+
+  def threes(roll)
+    #to add in later
+  end #end of threes method
+
+  def fours(roll)
+    #to add in later
+  end #end of fours method
+
+  def fives(roll)
+    #to add in later
+  end #end of fives method
+
+  def sixes(roll)
+    #to add in later
+  end #end of sixes method
+
+# Lower Section Methods
+   # def three_of_a_kind(roll)
+    # to add in later
+   # end #end of three of a kind method
+
+  # def four_of_a_kind(roll)
+    # to add in later
+  # end #end of four of a kind method
+
+  # def full_house(roll)
+    # to add in later
+  # end #end of full house method
+
+  # def small_straight(roll)
+    # to add in later
+  # end #end of small_straight method
+
+  # def large_straight(roll)
+    # to add in later
+  # end #end of large_straight method
+
+  # def yahtzee(roll)
+  # to add in later, repeatable?
+  # end #end of yahtzee method
+
+  # def chance(roll)
+    # to add in later
+  # end #end of chance method
+
+
+
 
 end #end of ScoreCard class!
 
@@ -129,6 +196,9 @@ end #end of ScoreCard class!
 
 game = Game.new
 game.turn
+#scorecard = ScoreCard.new
+#scorecard.testing #prints test scripts
+
 
 
 
