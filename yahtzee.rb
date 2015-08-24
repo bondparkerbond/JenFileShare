6181bbf288a9c0476de74b36e2e4ca1350afe951
JenJen1210 <jen.eutsler@gmail.com>
2015-08-24 11:53:51 -0600

    finish roll method

--- yahtzee.rb
+++ yahtzee.rb
@@ -1,33 +1,50 @@
-#Put Methods we will use
-#Set 5 arrays for 5 dice rolls.
-#players, scorecards, game classes
-#Give rule basics
-#roll dice
-unrolled = [1,2,3,4,5,6]
-rolled = []
-user1_choice = []
-user2_choice = []
-5.times do
-rolled << unrolled.sample
-end
-puts "You rolled: #{rolled}"
-puts "Type the dice you want to keep, separated by commas, no spaces"
-user1_choice = gets.strip.split(",")
-puts "#{user1_choice}" #take out for final code!!!
-#Roll # 2
-(5 - user1_choice.length).times do
-  user1_choice << unrolled.sample.to_s
-end
-puts "Your second roll is: #{user1_choice}"
-#Roll # 3
-puts "Type the dice you want to keep, separated by commas, no spaces"
-user1_choice = gets.strip.split(",")
-puts "#{user1_choice}" #take out for final code!!!
+class Game
+  attr_accessor :unrolled, :user1_choice
 
-(5 - user1_choice.length).times do
-  user1_choice << unrolled.sample.to_s
-end
-puts "Your final roll is: #{user1_choice}"
+  def initialize
+    @unrolled = [1,2,3,4,5,6]
+    @user1_choice = []
+  end
+
+  def roll
+    5.times do
+      @user1_choice << @unrolled.sample
+    end # goes to times loop
+    puts "You rolled: #{@user1_choice}"
+    puts "Type the dice you want to keep, separated by commas, no spaces"
+    @user1_choice = gets.strip.split(",")
+    puts "#{@user1_choice}" #take out for final code!!!
+    if @user1_choice.length != 5
+      (5 - @user1_choice.length).times do
+        @user1_choice << unrolled.sample.to_s
+      end #goes to times loop
+      puts "Your second roll is: #{@user1_choice}"
+      puts "Type the dice you want to keep, separated by commas, no spaces"
+      @user1_choice = gets.strip.split(",")
+      puts "#{@user1_choice}" #take out for final code!!!
+      if @user1_choice.length != 5
+        (5 - @user1_choice.length).times do
+          @user1_choice << unrolled.sample.to_s
+        end #times loop
+      else #exiting after second roll
+      end #for innermost if loop
+    else #for keeping first choice and done
+    end #outer if loop
+    puts "Your final roll is (all three choices): #{@user1_choice}"
+  end # goes to method
+end #end of game class
+
+game = Game.new
+game.roll
+
+
+
+
+#Roll # 2
+
+
+#Roll # 3
+
 
 
 #select which dice values to keep
