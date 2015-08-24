563ac0736c9616e4a05b232e301312de70f3534b
bondparkerbond <parkerbond@gmail.com>
2015-08-24 10:28:04 -0600

    init commit

---
+++ yahtzee.rb
@@ -0,0 +1,11 @@
+#Put Methods we will use
+#Set 5 arrays for 5 dice rolls.
+#players, scorecards, game classes
+#Give rule basics
+#roll dice
+#select which dice values to keep
+#other player goes
+#second roll
+#third roll
+#tally score
+#next turn
