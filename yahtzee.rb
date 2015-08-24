c0900b8c169f48e8e0c8fff8e2fc0d96ce162990
bondparkerbond <parkerbond@gmail.com>
2015-08-24 11:25:40 -0600

    roll mechanics

--- yahtzee.rb
+++ yahtzee.rb
@@ -3,6 +3,33 @@
 #players, scorecards, game classes
 #Give rule basics
 #roll dice
+unrolled = [1,2,3,4,5,6]
+rolled = []
+user1_choice = []
+user2_choice = []
+5.times do
+rolled << unrolled.sample
+end
+puts "You rolled: #{rolled}"
+puts "Type the dice you want to keep, separated by commas, no spaces"
+user1_choice = gets.strip.split(",")
+puts "#{user1_choice}" #take out for final code!!!
+#Roll # 2
+(5 - user1_choice.length).times do
+  user1_choice << unrolled.sample.to_s
+end
+puts "Your second roll is: #{user1_choice}"
+#Roll # 3
+puts "Type the dice you want to keep, separated by commas, no spaces"
+user1_choice = gets.strip.split(",")
+puts "#{user1_choice}" #take out for final code!!!
+
+(5 - user1_choice.length).times do
+  user1_choice << unrolled.sample.to_s
+end
+puts "Your final roll is: #{user1_choice}"
+
+
 #select which dice values to keep
 #other player goes
 #second roll
